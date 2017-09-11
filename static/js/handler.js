5e85c69179c30ece3293f6178ab0375733968aef
Vikram <vikram.denizen@gmail.com>
2017-09-11 16:34:18 +0800

    modify UI

--- static/js/handler.js
+++ static/js/handler.js
@@ -7,7 +7,9 @@ $("#getButton").click(function(){
 	if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition);
     } else { 
-        x.innerHTML = "Geolocation is not supported by this browser.";
+        
+        $('#country').text("Geolocation is not supported by this browser.");
+		
     }
 })
 
