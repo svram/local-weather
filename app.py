9713dd7824be8e31b65cd88fc5322baf3e875a9f
Vikram <vikram.denizen@gmail.com>
2017-09-11 11:10:05 +0800

    make changes to deploy to heroku

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from flask import Flask, render_template, session
 
+#UNCOMMENT IN PRODUCTION SERVER
+from flask_heroku import Heroku
+
+
+# create the application object
 app = Flask(__name__)
+app.config.from_object('_config')
+
+#UNCOMMENT IN PRODUCTION SERVER
+heroku = Heroku(app)
 
 @app.route('/')
 def index():
